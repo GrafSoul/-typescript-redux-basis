29c922384045bdda087ec02ee709c735849a778a
GrafSoul <grafsoul@gmail.com>
2021-12-28 18:49:12 +0500

    feat: Added useTypedSelector Hook

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,10 +1,18 @@
 import React, { useState } from "react";
+import { useActions } from "../hooks/useActions";
+import { useTypedSelector } from "../hooks/useTypedSelector";
 
 export const RepositoriesList: React.FC = () => {
   const [term, setTerm] = useState<string>("");
+  const { searchRepositories } = useActions();
+  const { data, loading, error } = useTypedSelector(
+    (state) => state.repositories
+  );
 
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    searchRepositories(term);
+    setTerm("");
   };
 
   return (
@@ -17,6 +25,10 @@ export const RepositoriesList: React.FC = () => {
         />
         <button>Search</button>
       </form>
+
+      {error && <h3>{error}</h3>}
+      {loading && <h3>Loading...</h3>}
+      {!error && !loading && data.map((name) => <div key={name}>{name}</div>)}
     </div>
   );
 };
