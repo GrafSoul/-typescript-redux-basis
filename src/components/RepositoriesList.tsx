6d73b745cb5121078e41a3fb93daf0fc61c5f2a3
GrafSoul <grafsoul@gmail.com>
2021-12-28 14:17:34 +0500

    feat: Added RepositoriesList component.

---
+++ src/components/RepositoriesList.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const RepositoriesList: React.FC = () => {
+  return (
+    <div>
+      <form>
+        <input type="text" />
+        <button>Search</button>
+      </form>
+    </div>
+  );
+};
+
+export default RepositoriesList;
