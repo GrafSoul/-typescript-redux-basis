aecd955fef10f0eef8017c472a4576078a32d361
GrafSoul <grafsoul@gmail.com>
2021-12-28 14:44:26 +0500

    feat: Added local state in the RepositoriesList component.

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const RepositoriesList: React.FC = () => {
+  const [term, setTerm] = useState<string>("");
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+
   return (
     <div>
-      <form>
-        <input type="text" />
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          value={term}
+          onChange={(e) => setTerm(e.target.value)}
+        />
         <button>Search</button>
       </form>
     </div>
